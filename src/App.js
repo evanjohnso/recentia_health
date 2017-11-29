15537cb45fca14c87aaab2f7128fe446c3fde4c6
ej <evanrjohnso@gmail.com>
2017-11-29 15:35:48 -0800

    add component folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Home from './Home';
+import Home from './Components/Home';
 import './App.css';
 import { Route, Switch, Link } from 'react-router-dom'
 
