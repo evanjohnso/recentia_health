8c8ec8fb0292a5506beb58c1849205d0cb5de3c7
ej <evanrjohnso@gmail.com>
2017-11-29 13:44:27 -0800

    add first form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,68 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      loggedIn: false
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.signInPage = this.signInPage.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.setState({
+      loggedIn: !this.state.loggedIn
+    });
+  }
+  signInPage() {
+    this.setState({
+      loggedIn: !this.state.loggedIn
+    });
+  }
+
+  signInForm() {
+    return (<form onSubmit={this.handleSubmit}>
+      <div>
+        <label>Username</label>
+        <input type="text"
+               placeholder="Username"
+               name="username"
+               value={this.state.username}
+               onChange={this.handleChange}
+              ></input>
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password"
+               placeholder="Please enter a password"
+               name="password"
+               value={this.state.password}
+               onChange={this.handleChange}
+               ></input>
+      </div>
+      <button type="submit">Login</button>
+    </form>)
+  }
+
+
+
+  anotherThing() {
+    return <button onClick={this.signInPage}>Sign In Page</button>
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -12,23 +73,14 @@ class App extends Component {
         </header>
         <h2>Welcome to Recentia Health Portal!</h2>
         <h4>Thesaurus for Medical Terms</h4>
+        <h1>{this.state.username}</h1>
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam commodo tincidunt nibh, ac vulputate eros tempus eu. Sed malesuada lobortis elit vel tincidunt. Nunc at nulla sit amet massa condimentum sagittis id vitae augue. Nullam placerat risus accumsan, aliquam augue sagittis, molestie neque. Proin a lacus tincidunt, molestie dolor a, placerat diam. Aenean dictum elit in mauris eleifend, quis feugiat arcu fermentum. Aliquam sapien nibh, ornare ut aliquam et, dictum a ligula. Etiam ac orci sodales, congue lacus eu, vulputate sapien. Nullam consectetur laoreet justo sit amet lacinia. Etiam suscipit augue et venenatis ultricies. Proin imperdiet ex vitae nisi varius suscipit eget at elit.
         </p>
-        <div>
-          <label>Username</label>
-          <input type="text"></input>
-        </div>
-        <div>
-          <label>Password</label>
-          <input type="text"></input>
-        </div>
-        <button type="submit">Login</button>
+        <p>{this.state.loggedIn ? this.anotherThing() : this.signInForm()}</p>
         <button>Create New Account</button>
         <p><a href="/">Forgot Password?</a></p>
       </div>
     );
   }
 }
-
-export default App;
